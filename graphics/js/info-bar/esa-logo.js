823d153e80e3f148aa08d8d61caf01dcb2b700a3
zoton2 <zoton2@gmail.com>
2018-02-14 00:51:41 +0000

    Adjust timing on ESA info bar logo cycle

--- graphics/js/info-bar/esa-logo.js
+++ graphics/js/info-bar/esa-logo.js
@@ -2,27 +2,26 @@
 
 // Declaring other variables.
 var esaBarLogoCurrentRotation = 0; // 0: normal - 1: hashtag
-var esaBarTicks = 0;
+var esaBarLogoTicks = 0;
 
 function changeESALogo() {
 	// JQuery selectors.
 	var smallLogo = $('#esaBarLogoContainer #esaSmallLogo');
 	var hashtag = $('#esaBarLogoContainer #esaHashtagLogo');
 	
-	// Logo will not change until 7th tick (30 seconds).
-	esaBarTicks++;
-	if (esaBarTicks < 7)
-		return;
-	else
-		esaBarTicks = 1;
+	esaBarLogoTicks++;
 	
-	// Change to hashtag logo after 30s.
-	if (esaBarLogoCurrentRotation === 0)
+	// Change to hashtag text after 50s.
+	if (esaBarLogoCurrentRotation === 0 && esaBarLogoTicks >= 11) {
 		animationFadeOutInElements(smallLogo, hashtag);
+		esaBarLogoTicks = 1;
+		esaBarLogoCurrentRotation ^= 1; // Toggle between 0 and 1.
+	}
 	
-	// Change to normal logo after 30s.
-	else
+	// Change to normal logo after 10s.
+	else if (esaBarLogoCurrentRotation === 1 && esaBarLogoTicks >= 3) {
 		animationFadeOutInElements(hashtag, smallLogo);
-	
-	esaBarLogoCurrentRotation ^= 1; // Toggle between 0 and 1.
+		esaBarLogoTicks = 1;
+		esaBarLogoCurrentRotation ^= 1; // Toggle between 0 and 1.
+	}
 }
