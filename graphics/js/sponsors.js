af95d12ca1987bd30fa2bde7e268df24a79bf8a6
zoton2 <zoton2@gmail.com>
2018-02-20 10:59:59 +0100

    Hardcode sponsors so Twitch/Elgato get more show

--- graphics/js/sponsors.js
+++ graphics/js/sponsors.js
@@ -4,10 +4,19 @@ $(() => {
 	var sponsorContainer = $('#sponsorContainer');
 	
 	var init = false;
+	var sponsorImagesModified = [];
 	var sponsorImages = nodecg.Replicant('assets:sponsors');
 	sponsorImages.on('change', newVal => {
+		// Insert Twitch/Elgato logos 3 times before anything else.
+		for (var i = 0; i < 3; i++) {
+			newVal.forEach(logo => {
+				if (i >= 2 || (i < 2 && (logo.name.toLowerCase().indexOf('twitch') >= 0 || logo.name.toLowerCase().indexOf('elgato') >= 0)))
+					sponsorImagesModified.push(logo);
+			});
+		}
+		
 		// If we aren't currently doing a rotation and there are logos available, start it off.
-		if (!init && newVal.length > 0) {
+		if (!init && sponsorImagesModified.length > 0) {
 			setInterval(rotateSponsors, 60000);
 			rotateSponsors();
 			init = true;
@@ -16,8 +25,8 @@ $(() => {
 	
 	var index = 0;
 	function rotateSponsors() {
-		animationChangeSponsorImage(sponsorContainer, sponsorImages.value[index].url);
+		animationChangeSponsorImage(sponsorContainer, sponsorImagesModified[index].url);
 		index++;
-		if (index >= sponsorImages.value.length) index = 0;
+		if (index >= sponsorImagesModified.length) index = 0;
 	}
 });
